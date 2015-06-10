4db305d5ff05504c16f5d57b042c78ee0bad3277
katata <katata@basicinc.jp>
2015-06-10 20:16:12 +0900

    ipaファイルの出力先が違うので修正

--- Scripts/distribute-deploygate.sh
+++ Scripts/distribute-deploygate.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -eu
 
+#--file Distribution/AdHoc/${APPNAME}.ipa
+
 bundle exec ipa distribute:deploygate \
-  --file Distribution/AdHoc/${APPNAME}.ipa \
+  --${APPNAME}.ipa \
   --api_token $DEPLOYGATE_API_TOKEN \
   --distribution_key $DEPLOYGATE_DISTRIBUTION_KEY \
   --release_note $RELEASE_NOTE \
