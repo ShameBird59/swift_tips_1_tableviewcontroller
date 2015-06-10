774e48c80d60f8ab4f31a23a3d9fe7aaaee8b22c
katata <katata@basicinc.jp>
2015-06-10 20:18:30 +0900

    ちょっとミス

--- Scripts/distribute-deploygate.sh
+++ Scripts/distribute-deploygate.sh
@@ -4,7 +4,7 @@ set -eu
 #--file Distribution/AdHoc/${APPNAME}.ipa
 
 bundle exec ipa distribute:deploygate \
-  --${APPNAME}.ipa \
+  --file ${APPNAME}.ipa \
   --api_token $DEPLOYGATE_API_TOKEN \
   --distribution_key $DEPLOYGATE_DISTRIBUTION_KEY \
   --release_note $RELEASE_NOTE \
