08132370674d6edd0ce5cc74ed6cd1196ee2a6fc
katata <katata@basicinc.jp>
2015-06-10 20:12:04 +0900

    xcodebuildに変更

--- Scripts/build-adhoc.sh
+++ Scripts/build-adhoc.sh
@@ -5,11 +5,16 @@ XCODE_SCHEME=$APPNAME
 
 bundle exec sudo chown :wheel /Library/Developer/CoreSimulator/Profiles/Runtimes/iOS\ *.simruntime/Contents/Resources/RuntimeRoot/usr/lib/dyld_sim
 
-bundle exec sudo ipa build \
-  --workspace "$XCODE_WORKSPACE" \
-  --scheme "$APPNAME" \
-  --configuration Release \
-  --destination Distribution/AdHoc \
-  --embed MobileProvisionings/${APPNAME}Adhoc.mobileprovision \
-  --identity "$DEVELOPER_NAME"
+#bundle exec sudo ipa build \
+#  --workspace "$XCODE_WORKSPACE" \
+#  --scheme "$APPNAME" \
+#  --configuration Release \
+#  --destination Distribution/AdHoc \
+#  --embed MobileProvisionings/${APPNAME}Adhoc.mobileprovision \
+#  --identity "$DEVELOPER_NAME"
 
+bundle exec xcodebuild -sdk iphoneos \
+  -project $APPNAME.xcodeproj -target $APPNAME \
+  -configuration Release build \
+  CODE_SIGN_IDENTITY='$DEVELOPER_NAME' \
+  PROVISIONING_PROFILE='$PROFILE_UUID'
