7bed3432e0b9e327e45d966b65e34b38305c0f76
katata <katata@basicinc.jp>
2015-06-10 20:40:33 +0900

    Release-iphoneosからのパスに変更

--- Scripts/distribute-deploygate.sh
+++ Scripts/distribute-deploygate.sh
@@ -4,8 +4,7 @@ set -eu
 #--file Distribution/AdHoc/${APPNAME}.ipa
 
 bundle exec ipa distribute:deploygate \
-  --source-dir ./build/Release-iphoneos \
-  --file ${APPNAME}.ipa \
+  --file Release-iphoneos/${APPNAME}.ipa \
   --api_token $DEPLOYGATE_API_TOKEN \
   --distribution_key $DEPLOYGATE_DISTRIBUTION_KEY \
   --release_note $RELEASE_NOTE \
