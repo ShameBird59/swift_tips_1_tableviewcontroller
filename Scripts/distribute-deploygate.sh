68820322aea833972bc3f9bf85da523f979edee0
katata <katata@basicinc.jp>
2015-06-10 20:37:14 +0900

    --source-dir追加

--- Scripts/distribute-deploygate.sh
+++ Scripts/distribute-deploygate.sh
@@ -4,7 +4,8 @@ set -eu
 #--file Distribution/AdHoc/${APPNAME}.ipa
 
 bundle exec ipa distribute:deploygate \
-  -f build/Release-iphoneos/$APPNAME.ipa \
+  --source-dir ./build/Release-iphoneos \
+  --file ${APPNAME}.ipa \
   --api_token $DEPLOYGATE_API_TOKEN \
   --distribution_key $DEPLOYGATE_DISTRIBUTION_KEY \
   --release_note $RELEASE_NOTE \
